8325343550c347dfba801b92be9996cffe2dfc09
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-09-14 16:04:40 +0100

    Login component with all functionality

--- src/Auth/Reducer/RootReducer.js
+++ src/Auth/Reducer/RootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { sessionReducer } from "redux-react-session";
-import user from './userReducer';
 
 
 const RootReducer = combineReducers({
     session: sessionReducer,
-    user,
 })
 
 export default RootReducer;
