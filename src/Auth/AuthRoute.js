8e8c804a60c28c5db5f648353d77da8385c10e0e
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2023-01-13 13:37:39 +0100

    Refractoring

--- src/Auth/AuthRoute.js
+++ src/Auth/AuthRoute.js
@@ -1,35 +1,35 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
-import { getUserDetails } from './Actions/userActions';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+import { getUserDetails } from "./Actions/userActions";
+import { useDispatch } from "react-redux";
 
 const AuthRoute = ({ component: Component, Failed, userStatus, ...rest }) => {
-    const dispatch = useDispatch();
-    useEffect(async () => {
-        dispatch(getUserDetails());
-    }, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserDetails());
+  }, []);
+  //   console.log(userStatus);
 
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                userStatus && !Failed.failed ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect to={{ pathname: '/sign_in' }} />
-                )
-            }
-        />
-    );
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        userStatus && !Failed.failed ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/sign_in" }} />
+        )
+      }
+    />
+  );
 };
 
 const mapStateToProps = state => {
-    return {
-        userStatus: state.user,
-        Failed: state.user,
-    };
+  return {
+    userStatus: state.user,
+    Failed: state.user,
+  };
 };
 
 // const mapDispatchToProps = dispatch => {
