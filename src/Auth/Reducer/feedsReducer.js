313abd7d945423feb893836b7f7d86a66f934737
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2023-01-16 16:43:18 +0100

    Refractoring

--- src/Auth/Reducer/feedsReducer.js
+++ src/Auth/Reducer/feedsReducer.js
@@ -1,10 +1,18 @@
-import { GET_ALL_FEEDS_SUCCESS } from '../Actions/types';
+import {
+    GET_ALL_FEEDS_SUCCESS,
+    GET_ALL_FEEDS_REQUEST,
+    GET_ALL_FEEDS_FAILURE,
+} from '../Actions/types';
 
 // eslint-disable-next-line
 export default function (state = {}, action) {
     switch (action.type) {
+        case GET_ALL_FEEDS_REQUEST:
+            return { ...state, feedsRequest: action.payload };
         case GET_ALL_FEEDS_SUCCESS:
             return { ...state, allFeeds: action.payload };
+        case GET_ALL_FEEDS_FAILURE:
+            return { ...state, Failled: action.payload };
 
         default:
             return state;
