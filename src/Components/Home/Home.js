374d85ecc72cc39ef8da4781ffb61d1bd7d6205e
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2023-05-10 12:01:45 +0100

    Refractoring home page for mobile devices

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css';
 import HomeImage from '../Assets/brohome.png';
+import connectImage from '../Assets/connect.png';
+
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -8,13 +10,26 @@ const Home = () => {
         <div>
             <div className="home_container">
                 <div className="home_left">
+                    <h3>Welcome to Staffconn</h3>
                     <h5>
                         Connect with your colleagues, share information and
                         yield more results.
                     </h5>
-                    <Link to="/sign_in" className="sign_in_button">
-                        <span>sign-in</span>
-                    </Link>
+                    <div className="connect__img">
+                        <img
+                            src={connectImage}
+                            className="connect_img"
+                            alt=""
+                        />
+                    </div>
+                    <div className="sign_in_up_btn">
+                        <Link to="/sign_in" className="sign_in_button">
+                            <span>Sign In</span>
+                        </Link>
+                        <Link to="/create_user" className="sign_up_button">
+                            <span>Sign Up</span>
+                        </Link>
+                    </div>
                 </div>
                 <div className="home_right">
                     <div className="home_right_container">
